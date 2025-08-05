cac24f2d50c368778557011a2fa2e943b3eaea2d
Whitney Hansen <41927897+WhitneyH1317@users.noreply.github.com>
2025-08-05 09:44:07 -0500

    Creating Lesson 2 script

---
+++ code/lesson02_answers.R
@@ -0,0 +1,13 @@
+
+
+# ---- DAY 2: EXERCISES ----
+
+rm(list = ls()) # clear out any existing data taking up room in your working environment
+
+# load relevant libraries
+library(terra)
+library(sf)
+library(mapview)
+library(dplyr)
+
+
